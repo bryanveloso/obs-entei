13d32fef429f950f5aa9efaa8a06cc34300e0f75
Bryan Veloso <bryan@velo.so>
2025-07-17 20:49:35 -0700

    feat: Implement actual libwebsockets connection functionality.

--- src/entei-provider.c
+++ src/entei-provider.c
@@ -25,6 +25,11 @@ static void *entei_caption_provider_create(obs_data_t *settings, obs_source_t *s
 	if (url && strlen(url) > 0) {
 		provider->websocket_url = strdup(url);
 		provider->client = websocket_client_create(url);
+		
+		if (provider->client) {
+			websocket_client_set_connect_callback(provider->client, on_websocket_connect, provider);
+			websocket_client_set_message_callback(provider->client, on_websocket_message, provider);
+		}
 	}
 	
 	obs_log(LOG_INFO, "Entei caption provider created");
@@ -47,21 +52,64 @@ static void entei_caption_provider_destroy(void *data)
 	free(provider);
 }
 
+static void on_websocket_connect(bool connected, void *user_data)
+{
+	struct entei_caption_provider *provider = (struct entei_caption_provider *)user_data;
+	
+	if (connected) {
+		obs_log(LOG_INFO, "Entei provider: WebSocket connected successfully");
+	} else {
+		obs_log(LOG_WARNING, "Entei provider: WebSocket connection failed or disconnected");
+	}
+}
+
+static void on_websocket_message(const char *message, size_t len, void *user_data)
+{
+	struct entei_caption_provider *provider = (struct entei_caption_provider *)user_data;
+	
+	obs_log(LOG_INFO, "Entei provider received message: %.*s", (int)len, message);
+	
+	// TODO: Parse Phoenix message and extract transcription text
+	// TODO: Forward to OBS caption system
+}
+
+static bool connect_button_clicked(obs_properties_t *props, obs_property_t *property, void *data)
+{
+	UNUSED_PARAMETER(props);
+	UNUSED_PARAMETER(property);
+	
+	struct entei_caption_provider *provider = (struct entei_caption_provider *)data;
+	
+	if (!provider->client) {
+		obs_log(LOG_ERROR, "No WebSocket client available");
+		return false;
+	}
+	
+	if (websocket_client_is_connected(provider->client)) {
+		obs_log(LOG_INFO, "Disconnecting from WebSocket server");
+		websocket_client_disconnect(provider->client);
+	} else {
+		obs_log(LOG_INFO, "Connecting to WebSocket server");
+		websocket_client_connect(provider->client);
+	}
+	
+	return true;
+}
+
 static obs_properties_t *entei_caption_provider_get_properties(void *data)
 {
-	UNUSED_PARAMETER(data);
-	
 	obs_properties_t *props = obs_properties_create();
 	
 	obs_properties_add_text(props, "websocket_url", "WebSocket URL", OBS_TEXT_DEFAULT);
 	obs_properties_add_text(props, "status", "Connection Status", OBS_TEXT_INFO);
+	obs_properties_add_button(props, "connect_button", "Connect/Disconnect", connect_button_clicked);
 	
 	return props;
 }
 
 static void entei_caption_provider_get_defaults(obs_data_t *settings)
 {
-	obs_data_set_default_string(settings, "websocket_url", "ws://localhost:7175/socket/websocket");
+	obs_data_set_default_string(settings, "websocket_url", "ws://saya:7175/socket/websocket");
 	obs_data_set_default_string(settings, "status", "Disconnected");
 }
 
--- src/websocket-client.h
+++ src/websocket-client.h
@@ -8,11 +8,17 @@ extern "C" {
 
 struct websocket_client;
 
+typedef void (*websocket_message_callback_t)(const char *message, size_t len, void *user_data);
+typedef void (*websocket_connect_callback_t)(bool connected, void *user_data);
+
 struct websocket_client *websocket_client_create(const char *url);
 void websocket_client_destroy(struct websocket_client *client);
 bool websocket_client_connect(struct websocket_client *client);
 void websocket_client_disconnect(struct websocket_client *client);
 bool websocket_client_is_connected(struct websocket_client *client);
+void websocket_client_send(struct websocket_client *client, const char *message);
+void websocket_client_set_message_callback(struct websocket_client *client, websocket_message_callback_t callback, void *user_data);
+void websocket_client_set_connect_callback(struct websocket_client *client, websocket_connect_callback_t callback, void *user_data);
 
 #ifdef __cplusplus
 }
